fb43417458da212f8ec3642eeada6460ff8d9fad
KusnirekAlexis <83014109+KusnirekAlexis@users.noreply.github.com>
2022-05-10 01:21:49 +0200

    commit progressif du projet partie 1

---
+++ Source/GhostHunter/GhostHunterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GhostHunterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHOSTHUNTER_API AGhostHunterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
